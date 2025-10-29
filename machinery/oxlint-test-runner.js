b204d244560606ed28ffd18174bf2a62acb2460c
AlbertSmit <albert@kaliber.net>
2025-10-29 10:18:16 +0100

    Update test run

--- machinery/oxlint-test-runner.js
+++ machinery/oxlint-test-runner.js
@@ -1,29 +1,48 @@
-const {
-  spawn
-} = require('child_process');
-const path = require('path');
-const fs = require('fs');
+const { spawn } = require('child_process')
+const path = require('path')
+const glob = require('glob')
 
-const rulesDir = path.resolve(__dirname, '../rules');
-const oxlintPath = path.resolve(__dirname, '../node_modules/.bin/oxlint');
+const rulesDir = path.resolve(__dirname, '../rules')
+const oxlintPath = path.resolve(__dirname, '../node_modules/.bin/oxlint')
 
-fs.readdirSync(rulesDir).forEach(rule => {
-  const ruleDir = path.resolve(rulesDir, rule);
-  const testFile = path.resolve(ruleDir, 'test.js');
-  if (fs.existsSync(testFile)) {
-    console.log(`Running tests for ${rule}...`);
-    const oxlint = spawn(oxlintPath, ['--config', '.oxlintrc.json', testFile]);
+const testFiles = glob.sync('**/test.js', { cwd: rulesDir })
+
+const runTest = (testFile) => {
+  return new Promise((resolve, reject) => {
+    const ruleDir = path.dirname(testFile)
+    const rule = path.basename(ruleDir)
+    console.log(`Running tests for ${rule}...`)
+    const oxlint = spawn(oxlintPath, ['--config', '.oxlintrc.json', path.resolve(rulesDir, testFile)])
+
+    let stdout = ''
+    let stderr = ''
 
     oxlint.stdout.on('data', (data) => {
-      console.log(data.toString());
-    });
+      stdout += data.toString()
+    })
 
     oxlint.stderr.on('data', (data) => {
-      console.error(data.toString());
-    });
+      stderr += data.toString()
+    })
 
     oxlint.on('close', (code) => {
-      console.log(`Tests for ${rule} finished with code ${code}`);
-    });
-  }
-});
+      if (code === 0) {
+        console.log(`Tests for ${rule} passed`)
+        resolve({ rule, stdout, stderr })
+      } else {
+        console.error(`Tests for ${rule} failed with code ${code}`)
+        reject({ rule, code, stdout, stderr })
+      }
+    })
+  })
+}
+
+Promise.all(testFiles.map(runTest))
+  .then((results) => {
+    console.log('All tests passed!')
+  })
+  .catch((error) => {
+    console.error('Some tests failed:')
+    console.error(error)
+    process.exit(1)
+  })
